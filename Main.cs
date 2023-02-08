ed984baa95c304d591bf0a38cad0b88c9ace165c
Oren Sayag <ext.oren.s@medica.local>
2023-02-08 17:24:12 +0200

    first commit

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using IConduct.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medica.IConductPlugins
+{
+    public class EmptyPluginTest : IPlugin
+    {
+        public bool Execute(PluginParam param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
